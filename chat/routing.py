470bbc87d2d0a6afcb6109a55999c59176ed4a4c
Grapsinho <92812712+Grapsinho@users.noreply.github.com>
2024-05-14 12:12:55 -0700

    Update routing.py

--- chat/routing.py
+++ chat/routing.py
@@ -2,6 +2,6 @@ from django.urls import path, re_path
 from .consumers import ChatConsumer, DeleteMessageConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<chat_pk>\d+)/$', ChatConsumer.as_asgi()),
-    re_path(r'ws/chat/delete_message/(?P<chat_pk>\d+)/$', DeleteMessageConsumer.as_asgi()),
+    re_path(r'wss/chat/(?P<chat_pk>\d+)/$', ChatConsumer.as_asgi()),
+    re_path(r'wss/chat/delete_message/(?P<chat_pk>\d+)/$', DeleteMessageConsumer.as_asgi()),
 ]
