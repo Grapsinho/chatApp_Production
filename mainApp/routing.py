d7f4df253ff5a86d88f5afd8e0ca2db9e4b37456
Grapsinho <92812712+Grapsinho@users.noreply.github.com>
2024-05-14 12:13:46 -0700

    Update routing.py

--- mainApp/routing.py
+++ mainApp/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/live_search/$', consumers.LiveSearchConsumer.as_asgi()),
+    re_path(r'wss/live_search/$', consumers.LiveSearchConsumer.as_asgi()),
 ]
