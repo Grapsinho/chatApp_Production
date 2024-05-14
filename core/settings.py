aaeafa336e839103df54c8b93f81d5228d3e0775
Grapsinho <92812712+Grapsinho@users.noreply.github.com>
2024-05-14 13:55:05 -0700

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ secret_key = os.environ.get('KEY_SECRET')
 SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['chatapp-production-jbpi.onrender.com', 'localhost']
 
