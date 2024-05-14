591ddea76f78bedad6c08620fd69323f32c42ec7
Grapsinho <92812712+Grapsinho@users.noreply.github.com>
2024-05-14 12:18:59 -0700

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -154,7 +154,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("localhost", 6379)],
+            "hosts": [external_redis],
         },
     },
 }
